394efc26f4eff366edbede3c0fe55983be679737
vxnicios <vviniciosc@gmail.com>
2023-04-19 04:28:41 -0300

    input estilizado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,22 @@
 import {products} from './productsData.js'
 import {renderCards} from './render.js'
-import {handleDarkMode} from './darkMode.js'
+import {handleDarkMode} from './theme.js'
+
+
+
+const sliderStyle = () => {
+    const slider = document.querySelector('.filter__input input')
+    
+    slider.addEventListener('input', () => {
+
+        const value = ((slider.value - slider.min) / (slider.max - slider.min) * 100 )
+        const progressBarColor = `linear-gradient(90deg, var(--color-brand-1) ${value}%, var(--color-grey-5) ${value}%)`
+        slider.style.background = progressBarColor
+
+    })
+}
+
 
 renderCards(products)
 handleDarkMode()
+sliderStyle()
--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,24 @@
-/* Desenvolva sua lógica aqui ... */
+export const handleDarkMode = () => {
+
+    const button = document.querySelector('.dark-mode__btn')
+    const buttonImg = document.querySelector('.dark-mode__btn > img')
+    const root = document.querySelector('html')
+
+
+    button.addEventListener('click', () => {
+
+        root.classList.toggle('dark__mode')
+
+
+        if (root.classList.contains('dark__mode')) {
+
+            buttonImg.src = './src/assets/vectors/sun.svg'
+            
+
+        } else {
+
+            buttonImg.src = './src/assets/vectors/moon.svg'
+
+        }
+    })
+}
